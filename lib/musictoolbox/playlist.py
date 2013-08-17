90784ad062c19f5b9db7288d02766d39edf64b8e
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2013-08-17 00:56:08 -0700

    add playlist creator proper command line parameters

--- lib/musictoolbox/playlist.py
+++ lib/musictoolbox/playlist.py
@@ -2,6 +2,8 @@ import mutagen
 import glob
 import os
 import sys
+import logging
+import argparse
 
 def sort_by_track_number(paths):
     metadatas = [ (f,mutagen.File(f)) for f in paths ]
@@ -9,9 +11,8 @@ def sort_by_track_number(paths):
     relevant = list(sorted(relevant))
     return [ f for x,f,w in relevant ]
 
-def make_album_playlist(playlistpath, *paths):
+def make_album_playlist(playlistpath, paths, dryrun=False):
     playlist_dir = os.path.dirname(playlistpath)
-    playlist_fileobj = file(playlistpath, 'wb')
     files  = []
     for p in paths:
         if os.path.isdir(p):
@@ -21,14 +22,39 @@ def make_album_playlist(playlistpath, *paths):
             files.append(p)
     sortedfiles = sort_by_track_number(files)
     relativefiles = [ os.path.relpath(f, playlist_dir) for f in sortedfiles ]
+    if dryrun:
+        playlist_fileobj = sys.stdout
+    else:
+        playlist_fileobj = file(playlistpath, 'wb')
     playlist_fileobj.write("\n".join(relativefiles))
 
-def main():
-    parms = sys.argv[1:]
-    paths = parms[:-1]
-    playlistpath = parms[-1]
-    make_album_playlist(playlistpath, *paths)
+def get_parser():
+    program_name = os.path.basename(sys.argv[0])
+    program_shortdesc = "Creates a playlist from passed directories and files, sorted by track number"
+
+    parser = argparse.ArgumentParser(
+        prog=program_name,
+        description=program_shortdesc,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument("-n", "--dry-run", dest="dryrun", action="store_true", help="do nothing except show what will happen [default: %(default)s]")
+    parser.add_argument(dest="paths", help="files and directories to include in playlist", nargs='+')
+    parser.add_argument(dest="playlist", help="path to an M3U playlist to write", nargs=1)
+    return parser
+
+def main(argv=None):
+    logging.basicConfig(level=logging.WARNING)
+
+    if argv is None:
+        argv = sys.argv
+    else:
+        sys.argv.extend(argv)
+
+    parser = get_parser()
+    args = parser.parse_args()
+
+    make_album_playlist(args.playlist[0], args.paths, dryrun=args.dryrun)
     return 0
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(main(sys.argv))
